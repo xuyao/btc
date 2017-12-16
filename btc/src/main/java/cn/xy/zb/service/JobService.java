3d2a5a59f6846f98a3b0f49df6c3a7659e18637d
admin <admin@android-518657y899813ebc>
2017-12-16 22:02:04 +0800

    Signed-off-by: admin <admin@android-518657y899813ebc>

--- btc/src/main/java/cn/xy/zb/service/JobService.java
+++ btc/src/main/java/cn/xy/zb/service/JobService.java
@@ -23,19 +23,28 @@ public class JobService {
 	OrderService orderService;
 	
 	public void work(){
+//		long a = System.currentTimeMillis();
+		detail("btc_qc","btc_usdt");
+		detail("bcc_qc","bcc_usdt");
+		detail("ubtc_qc","ubtc_usdt");
 		detail("bts_qc","bts_usdt");
-//		detail("btc_qc","btc_usdt");
 		detail("xrp_qc","xrp_usdt");
 		detail("hsr_qc","hsr_usdt");
 		detail("eos_qc","eos_usdt");
-		detail("bcc_qc","bcc_usdt");
 		detail("ltc_qc","ltc_usdt");
 		detail("eth_qc","eth_usdt");
 		detail("etc_qc","etc_usdt");
 		detail("qtum_qc","qtum_usdt");
 		detail("bcd_qc","bcd_usdt");
 		detail("dash_qc","dash_usdt");
+		detail("sbtc_qc","sbtc_usdt");
+		detail("ink_qc","ink_usdt");
+		detail("tv_qc","tv_usdt");
+		detail("bcx_qc","bcx_usdt");
+		detail("bth_qc","bth_usdt");
+		detail("lbtc_qc","lbtc_usdt");
 		
+//		System.out.println((System.currentTimeMillis()-a)/1000);
 		
 //		AccountInfo ai = compService.getAccountInfo();
 //		System.out.println(ai.getQcAvailable()+" "+ai.getUsdtAvailable());
--- btc/src/main/java/cn/xy/zb/service/OrderService.java
+++ btc/src/main/java/cn/xy/zb/service/OrderService.java
@@ -15,6 +15,7 @@ import cn.xy.zb.util.NumberUtil;
 import cn.xy.zb.vo.AccountInfo;
 import cn.xy.zb.vo.AskBid;
 import cn.xy.zb.vo.Deal;
+import cn.xy.zb.vo.Result;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -38,20 +39,21 @@ public class OrderService {
 		Double buyPrice = deal.getBuyPrice();
 		Double sellPrice = deal.getSellPrice();
 		sellPrice = NumberUtil.doubleMul(sellPrice, usd_cny);
-		System.out.println("*****************************************");
 
 		Double amount = qc_limit/buyPrice;//qc的价格折算的数量
 		if(amount>deal.getBuyAmount())//如果限制仓位下的amount小于挂单买入的amount，以小的为准
 			amount=deal.getBuyAmount();
 		amount =  getAmount(deal.getBuyMarket(), amount);//买入量按照市场进行小数点转换
 		
+//		doOrder(deal, amount);
+		System.out.println("qc*****************************************"+amount);
 		StringBuilder sb = new StringBuilder(DateUtil.formatLongPattern(new Date())).append("\n");
 		sb.append(" 市场：").append(deal.getBuyMarket());
 		sb.append(" ").append(deal.getBuyPrice()).append(" ").append(buyPrice);
 		sb.append(" ").append(deal.getBuyAmount());
 		sb.append("\n");
 		sb.append(" 市场：").append(deal.getSellMarket());
-		sb.append(" ").append(deal.getSellPrice()).append(" ").append(sellPrice);;
+		sb.append(" ").append(deal.getSellPrice()).append(" ").append(sellPrice);
 		sb.append(" ").append(deal.getSellAmount());
 		sb.append("\n");
 		sb.append(" 买入价格：").append(deal.getBuyPrice()).append(" 卖出价格：").append(deal.getSellPrice()).append(" ").append(sellPrice);
@@ -67,12 +69,14 @@ public class OrderService {
 		Double buyPrice = deal.getBuyPrice();
 		Double sellPrice = deal.getSellPrice();
 		buyPrice = NumberUtil.doubleMul(buyPrice, usd_cny);
-		System.out.println("*****************************************");
 
 		Double amount = qc_limit/buyPrice;//qc的价格折算的数量
 		if(amount>deal.getBuyAmount())//如果限制仓位下的amount小于挂单买入的amount，以小的为准
 			amount=deal.getBuyAmount();
 		amount =  getAmount(deal.getBuyMarket(), amount);//买入量按照市场进行小数点转换
+		
+//		doOrder(deal, amount);
+		System.out.println("usdt*****************************************"+amount);
 		
 		StringBuilder sb = new StringBuilder(DateUtil.formatLongPattern(new Date())).append("\n");
 		sb.append(" 市场：").append(deal.getBuyMarket());
@@ -80,7 +84,7 @@ public class OrderService {
 		sb.append(" ").append(deal.getBuyAmount());
 		sb.append("\n");
 		sb.append(" 市场：").append(deal.getSellMarket());
-		sb.append(" ").append(deal.getSellPrice()).append(" ").append(sellPrice);;
+		sb.append(" ").append(deal.getSellPrice()).append(" ").append(sellPrice);
 		sb.append(" ").append(deal.getSellAmount());
 		sb.append("\n");
 		sb.append(" 买入价格：").append(deal.getBuyPrice()).append(" ").append(buyPrice).append(" 卖出价格：").append(deal.getSellPrice());
@@ -95,8 +99,30 @@ public class OrderService {
 	}
 	
 	
+	//循环等待请求
+	public void doOrder(Deal deal, Double amount){
+		Result buyResult = order(deal.getBuyMarket(), "1", String.valueOf(deal.getBuyPrice()),String.valueOf(amount));//买入
+		if(buyResult!=null){//请求接口成功
+			if("1000".equals(buyResult.getCode())){
+				int i=0;
+				do{
+					try {
+						Thread.sleep(320);//现成休眠320毫秒，等待买入成功
+						Result sellResult = order(deal.getSellMarket(), "0", String.valueOf(deal.getSellPrice()),String.valueOf(amount));//卖出
+						System.out.println("sellResult code:"+sellResult.getCode());
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					i++;
+				}while("1000".equals(i>7));//6次
+			}
+		}
+	}
+	
+	
 	//下单 tradeType交易类型1/0[buy/sell]
-	public void order(String currency, String tradeType,String price, String amount){
+	public Result order(String currency, String tradeType,String price, String amount){
+		Result result = null;
 		try {
 			Map<String, String> params = new HashMap<String, String>();
 			params.put("method", "order");
@@ -107,9 +133,12 @@ public class OrderService {
 			// 请求测试
 			String json = httpService.getJsonPost(params);
 			System.out.println("testOrder 结果: " + json);
+			JSONObject jsonObj = JSONObject.parseObject(json);
+			result = jsonObj.parseObject(json, Result.class);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+		return result;
 	}
 	
 
