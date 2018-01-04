c959b440eab3740eb8f26120fafbeff252830c36
xuyao <58394322@qq.com>
2018-01-04 15:44:49 +0800

    Signed-off-by: xuyao <58394322@qq.com>

--- btc/src/main/java/cn/xy/exx/RunCancel.java
+++ btc/src/main/java/cn/xy/exx/RunCancel.java
@@ -6,9 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class RunCancel {
 
 	public static void main(String[] args){
-		System.out.println("run zb order cancel start!!!");
+		System.out.println("run exx order cancel start!!!");
 		Market.init();//市场先加载
-		Tax.init();//税后加载
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContentCancelExx.xml");
 	}
 }
