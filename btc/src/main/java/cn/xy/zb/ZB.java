fcdc7aaf096fc90eb72b8e9c64a44176bf3160ff
xuyao <58394322@qq.com>
2018-02-12 15:08:16 +0800

    Signed-off-by: xuyao <58394322@qq.com>

--- btc/src/main/java/cn/xy/zb/ZB.java
+++ btc/src/main/java/cn/xy/zb/ZB.java
@@ -6,16 +6,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import cn.xy.zb.service.CompService;
 import cn.xy.zb.service.OrderService;
 
-public class Run {
-
+public class ZB {
 	public static void main(String[] args){
 		System.out.println("run zb start!!!");
 		Market.init();//市场先加载
-//		Tax.init();//税后加载
+//		zb 记得下单order，卖出时候只跑一次，其他都是三次
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContent.xml");
 		CompService compService = (CompService)context.getBean("compService");
 		OrderService orderService = (OrderService)context.getBean("orderService");
-		compService.usd_cny = compService.getTicker("usdt_qc").getLast();
-		orderService.usd_cny = compService.getTicker("usdt_qc").getLast();
 	}
 }
--- btc/src/main/java/cn/xy/zb/service/CompService.java
+++ btc/src/main/java/cn/xy/zb/service/CompService.java
@@ -11,6 +11,7 @@ import org.springframework.util.StringUtils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.plato.common.cache.memcached.MemcachedCache;
 
 import cn.xy.zb.util.ConstsUtil;
 import cn.xy.zb.vo.AccountInfo;
@@ -23,8 +24,9 @@ public class CompService extends LogService{
 
 	@Autowired
 	HttpService httpService;
+	MemcachedCache memcachedClient = MemcacheFactory.getClient();
 	
-	public Double usd_cny = ConstsUtil.getCnyUsd();//汇率
+	public Double usd_cny = (Double)memcachedClient.get("hl");//汇率
 	Double comp_cny_usd = ConstsUtil.getCompCnyUsd();//人民币比美元
 	Double comp_usd_cny = ConstsUtil.getCompUsdCny();//美元比人民币
 	
@@ -78,13 +80,13 @@ public class CompService extends LogService{
 		JSONArray bids2 = bidsArr.getJSONArray(1);
 		
 		AskBid ab = new AskBid();
-		ab.setAsk1(asks1.getDouble(0));
+		ab.setAsk1(asks1.getDouble(0));//卖二
 		ab.setAsk1_amount(asks1.getDouble(1));
-		ab.setAsk2(asks2.getDouble(0));
+		ab.setAsk2(asks2.getDouble(0));//卖一
 		ab.setAsk2_amount(asks2.getDouble(1));
-		ab.setBid1(bids1.getDouble(0));
+		ab.setBid1(bids1.getDouble(0));//买一
 		ab.setBid1_amount(bids1.getDouble(1));
-		ab.setBid2(bids2.getDouble(0));
+		ab.setBid2(bids2.getDouble(0));//买二
 		ab.setBid2_amount(bids2.getDouble(1));
 		ab.setMarket(market);
 		return ab;
@@ -93,20 +95,21 @@ public class CompService extends LogService{
 	
 	//嗅探
 	public Double sniffCnyUsd(AskBid ab1, AskBid ab2){
-		Double d = (ab2.getBid1()*usd_cny)/ab1.getAsk1();
+		Double d = (ab2.getBid1()*usd_cny)/ab1.getAsk2();
 		return d;
 	}
 	
 	//嗅探
 	public Double sniffUsdCny(AskBid ab1, AskBid ab2){
-		Double d = ab2.getBid1()/(ab1.getAsk1()*usd_cny);
+		Double d = ab2.getBid1()/(ab1.getAsk2()*usd_cny);
 		return d;
 	}
 	
 	//比较两个市场的套利价格,从ab1买，去ab2卖，第一个参数是人民币，第二个参数是usd
 	public Deal compCnyUsd(AskBid ab1, AskBid ab2){
 		Deal deal =null;
-		if((ab2.getBid1()*usd_cny)/ab1.getAsk1()>comp_cny_usd){//如果价格之差大于ab2的1.2%认为有利可图,后改成千分之6
+		
+		if((ab2.getBid1()*usd_cny)/ab1.getAsk1()>comp_cny_usd){//买一和卖二比价
 			deal = new Deal();
 			deal.setBuyPrice(ab1.getAsk1());//买入价格设置为ab1的卖一价格
 			deal.setSellPrice(ab2.getBid1());//卖出价格设置为ab2的买一价格
@@ -115,6 +118,31 @@ public class CompService extends LogService{
 			deal.setBuyAmount(Math.min(ab1.getAsk1_amount(), ab2.getBid1_amount()));//量取小的那个
 			deal.setSellAmount(Math.min(ab1.getAsk1_amount(), ab2.getBid1_amount()));//同上，买卖量相同
 			deal.setSellMarket(ab2.getMarket());
+			return deal;
+		}
+		
+		if((ab2.getBid2()*usd_cny)/ab1.getAsk2()>comp_cny_usd){//买二和卖一比价
+			deal = new Deal();
+			deal.setBuyPrice(ab1.getAsk2());//买入价格设置为ab1的卖一价格
+			deal.setSellPrice(ab2.getBid2());//卖出价格设置为ab2的买一价格
+			deal.setBuyMarket(ab1.getMarket());
+			
+			deal.setBuyAmount(Math.min(ab1.getAsk2_amount(), ab2.getBid2_amount()));//量取小的那个
+			deal.setSellAmount(Math.min(ab1.getAsk2_amount(), ab2.getBid2_amount()));//同上，买卖量相同
+			deal.setSellMarket(ab2.getMarket());
+			return deal;
+		}
+		
+		if((ab2.getBid1()*usd_cny)/ab1.getAsk2()>comp_cny_usd){//买一和卖一比价
+			deal = new Deal();
+			deal.setBuyPrice(ab1.getAsk2());//买入价格设置为ab1的卖一价格
+			deal.setSellPrice(ab2.getBid1());//卖出价格设置为ab2的买一价格
+			deal.setBuyMarket(ab1.getMarket());
+			
+			deal.setBuyAmount(Math.min(ab1.getAsk2_amount(), ab2.getBid1_amount()));//量取小的那个
+			deal.setSellAmount(Math.min(ab1.getAsk2_amount(), ab2.getBid1_amount()));//同上，买卖量相同
+			deal.setSellMarket(ab2.getMarket());
+			return deal;
 		}
 		return deal;
 	}
@@ -123,7 +151,8 @@ public class CompService extends LogService{
 	//第一个参数是usd，第二个参数是人民币
 	public Deal compUsdCny(AskBid ab1, AskBid ab2){
 		Deal deal =null;
-		if(ab2.getBid1()/(ab1.getAsk1()*usd_cny)>comp_usd_cny){//如果价格之差大于ab2的1.2%认为有利可图，后改成2.7%
+		
+		if(ab2.getBid1()/(ab1.getAsk1()*usd_cny)>comp_usd_cny){//买一对卖二
 			deal = new Deal();
 			deal.setBuyPrice(ab1.getAsk1());//买入价格设置为ab1的卖一价格
 			deal.setSellPrice(ab2.getBid1());//卖出价格设置为ab2的买一价格
@@ -132,6 +161,31 @@ public class CompService extends LogService{
 			deal.setBuyAmount(Math.min(ab1.getAsk1_amount(), ab2.getBid1_amount()));//量取小的那个
 			deal.setSellAmount(Math.min(ab1.getAsk1_amount(), ab2.getBid1_amount()));//同上，买卖量相同
 			deal.setSellMarket(ab2.getMarket());
+			return deal;
+		}
+		
+		if(ab2.getBid2()/(ab1.getAsk2()*usd_cny)>comp_usd_cny){//买二对卖一
+			deal = new Deal();
+			deal.setBuyPrice(ab1.getAsk2());//买入价格设置为ab1的卖一价格
+			deal.setSellPrice(ab2.getBid2());//卖出价格设置为ab2的买一价格
+			deal.setBuyMarket(ab1.getMarket());
+			
+			deal.setBuyAmount(Math.min(ab1.getAsk2_amount(), ab2.getBid2_amount()));//量取小的那个
+			deal.setSellAmount(Math.min(ab1.getAsk2_amount(), ab2.getBid2_amount()));//同上，买卖量相同
+			deal.setSellMarket(ab2.getMarket());
+			return deal;
+		}
+		
+		if(ab2.getBid1()/(ab1.getAsk2()*usd_cny)>comp_usd_cny){//买一对卖一
+			deal = new Deal();
+			deal.setBuyPrice(ab1.getAsk2());//买入价格设置为ab1的卖一价格
+			deal.setSellPrice(ab2.getBid1());//卖出价格设置为ab2的买一价格
+			deal.setBuyMarket(ab1.getMarket());
+			
+			deal.setBuyAmount(Math.min(ab1.getAsk2_amount(), ab2.getBid1_amount()));//量取小的那个
+			deal.setSellAmount(Math.min(ab1.getAsk2_amount(), ab2.getBid1_amount()));//同上，买卖量相同
+			deal.setSellMarket(ab2.getMarket());
+			return deal;
 		}
 		return deal;
 	}
